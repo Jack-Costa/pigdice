2fb64fcca55507edfa0fd8a47e42501711fbbbdb
Jack-Costa <Jackcost@umich.edu>
2025-02-11 11:38:31 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,59 +2,91 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+void play_game();
+void display_rules();
+void take_turn(int& score, int& score_this_turn, int& turn, bool& turn_over, bool& game_over);
+void roll(int& score_this_turn, bool& turn_over);
+void hold(int& score, int& score_this_turn, bool& turn_over, bool& game_over);
+
+
+
 int main() {
+    display_rules();
+    play_game();
+
+};
+void display_rules(){
     cout << "Let's Play PIG Dice!" << endl;
     cout << "* See how many turns it takes you to get to 20." << endl;
     cout << "*Turn ends when you hold or roll a 1." << endl;
     cout << "*If you roll a 1, you lose all points for the turn." << endl;
     cout << "*If you hold, you save all points for the turn" << endl;
     cout << endl;
+    cout << "turn: 1" << endl;
+};
+void play_game(){
     bool game_over = false;
     bool turn_over = false;
-    int turn_count = 1;
-    int die_val = 0;
-    char choice = 'a';
+    int turn = 1;
     int score_this_turn = 0;
-    int game_score = 0;
+    int score = 0;
+    while (game_over == false) {
+        take_turn(score,score_this_turn, turn, turn_over, game_over);
+    }
 
-    while (game_over == false){
-        if (game_score <=19 ) {
-            turn_over = false;
-            cout << "Turn " << turn_count << endl;
-            turn_count ++;
+    if(game_over == true) {
+        cout << endl;
+        cout << "You finished with a final score of 20 or more in " << turn << " turns!" << endl;
+    }
+
+};
+void take_turn(int& score, int& score_this_turn, int& turn, bool& turn_over, bool& game_over){
+    turn_over = false;
+    char choice = 'a';
+    cout << "roll or hold? (r/h): ";
+    cin >> choice;
+    if (choice == 'r') {
+        roll(score_this_turn, turn_over);
+        if (turn_over == true) {
+            turn ++;
+            cout << "turn: " << turn << endl;
         }
-        else if (game_score >= 20) {
-            game_over = true;
-            cout << endl;
-            cout << "You finished with a final score of 20 or more in " << turn_count << " turns!" << endl;
+    }
+    else if (choice == 'h') {
+        hold( score, score_this_turn, turn_over, game_over);
+        if (score < 20) {
+            turn ++;
+            cout << "turn: " << turn << endl;
         }
+    }
+    else {
+        cout << "Invalid choice! Try again." << endl;
+
+    }
+};
+void roll(int& score_this_turn, bool& turn_over){
+    int die;
+    srand(time(0));
+    die =rand() % 6 +1;
+    cout << "Die: "<< die << endl;
+    score_this_turn = score_this_turn + die;
+    if (die == 1) {
+        cout << "Turn over. No score."<< endl;
+        cout << endl;
         score_this_turn = 0;
-        while (turn_over == false) {
-            cout << "roll or hold? (r/h): ";
-            cin >> choice;
-            if (choice == 'r') {
-                srand(time(0));
-                die_val =rand() % 6 +1;
-                cout << "Die: "<< die_val << endl;
-                score_this_turn = score_this_turn + die_val;
-                if (die_val == 1) {
-                    cout << "Turn over. No score."<< endl;
-                    cout << endl;
-                    turn_over = true;
-                }
-            }
-            else if (choice == 'h' || turn_over == true) {
-                game_score = game_score + score_this_turn;
-                cout << "Score for turn:"<< score_this_turn << endl;
-                cout << "Game score: " << game_score << endl;
-                cout << endl;
-                score_this_turn = 0;
-                turn_over = true;
-            }
-            else {
-                cout << "Invalid choice! Try again." << endl;
-            }
-        };
-    };
-    return 0;
+        turn_over = true;
+
+    }
+
+};
+void hold(int& score, int& score_this_turn, bool& turn_over, bool& game_over){
+    score = score + score_this_turn;
+    cout << "Score for turn:"<< score_this_turn << endl;
+    cout << "Game score: " << score << endl;
+    cout << endl;
+    score_this_turn = 0;
+    turn_over = true;
+    if (score >= 20) {
+        game_over = true;
+    }
 }
